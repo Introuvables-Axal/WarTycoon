c18bb9fc7687b6c4191600e845cd4b03e0616614
Introuvables-Axal <fornitebattlepasseomg@gmail.com>
2025-10-08 21:00:45 +0200

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,18 @@
+{
+    "5286749994": [
+        {
+            "last_update": 1759048583,
+            "name": "aftermath: full",
+            "status": "use at own risk",
+            "url": "https://raw.githubusercontent.com/Introuvables-Axal/WarTycoon/refs/heads/main/test.lua",
+            "confirm_load": "return true  --local core = game:GetService(\"ReplicatedFirst\"):FindFirstChild(\"Core\"); return (core and not not core:FindFirstChild(\"LobbyClientGame\")) or \"priv9.ru - security measure\\\\n\\\\nYou must load aftermath in lobby.\""
+        },
+        {
+            "last_update": 1752251003,
+            "name": "aftermath: lite",
+            "status": "undetected",
+            "url": "https://svo.monster/assets/aftermath_lite.lua",
+            "confirm_load": "return nil"
+        }
+    ]
+}
